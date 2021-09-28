1ee189897530d7c27a5090a89812132fbe656d3b
SlowV <quocviet.hn98@gmail.com>
2021-09-28 23:21:48 +0700

    update height nha cai

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -2,5 +2,11 @@ $(document).ready(() => {
     $('.button-navbar').click(() => {
         $('.button-navbar').toggleClass('active');
         $('#mainnav-mobi').toggleClass('d-none');
-    })
+    });
+
+    setTimeout(() => {
+        if ($(window).width() >= 768) {
+            $('.nha-cai').css('height', $('#newList').outerHeight())
+        }
+    }, 100);
 })
